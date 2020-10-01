d031b9cae9a64600c253ddb65d6c360eec610cc2
szandany <44092058+szandany@users.noreply.github.com>
2020-09-30 23:06:50 -0500

    Update rawToChangeLog.sh

--- rawToChangeLog.sh
+++ rawToChangeLog.sh
@@ -1,10 +1,10 @@
 #/path/to/rawToChangeLog.sh /path/to/plain_sql/folder /path/to/new/generated/<changeLog Name>.sql
 
-echo '--liquibase formatted sql' > $2;
+echo '-- liquibase formatted sql' > $2;
 i=0
 find $1 -type f \( -iname "*.sql" ! -iname $2 \) -print |sort -n| while read filename; do
     i=$((i+1))
-    echo -e "\n--changeset "author"$i:${filename}"
+    echo -e "\n-- changeset "author"$i:${filename}"
     cat "$filename"
-    echo -e "\n--rollback \n"
+    echo -e "\n-- rollback \n"
 done >> $2
