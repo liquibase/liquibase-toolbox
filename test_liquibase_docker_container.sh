4cebc43a3df6bd4b8306f725582e8589a085e5ba
SteveZ <szandany@datical.com>
2021-01-15 14:24:59 -0600

    changeLog updated

--- test_liquibase_docker_container.sh
+++ test_liquibase_docker_container.sh
@@ -4,7 +4,9 @@ cwd=$(pwd)
 if [ -z "$2" ]
 then
       tput setaf 2;echo "For optional parameters, you can try: /path/to/test_liquibase_docker_container.sh <docker tag name> <path to changelog> <'liquibase flags'> <liquibase command>
-For example: /path/to/test_liquibase_docker_container.sh latest ./CLI-Projects/H2/myChangeLog.xml '--url=jdbc:h2:file:./h2tutorial --username=admin --password=password' updateSQL";tput sgr0
+For example: /path/to/test_liquibase_docker_container.sh latest ./CLI-Projects/H2/myChangeLog.xml '--url=jdbc:h2:file:./h2tutorial --username=admin --password=password' updateSQL
+or
+/path/to/test_liquibase_docker_container.sh latest ./CLI-Projects/H2/myChangeLog.xml --defaultsFile=changelog/liquibase.properties updateSQL";tput sgr0
       mkdir ./docker_changelogs
       echo -e "-- liquibase formatted sql\n\n-- changeset Tsvi:123\nCREATE TABLE CUSTOMER (ID INT NOT NULL, NAME VARCHAR(50) NOT NULL, AGE INT, LASTNAME VARCHAR(50) NOT NULL, YEAR INT NOT NULL, CONSTRAINT PK_CUSTOMER PRIMARY KEY (ID, YEAR));\n--rollback DROP TABLE CUSTOMER;" > ./docker_changelogs/changeLog.h2.sql
       changelogPath="./docker_changelogs/changeLog.h2.sql"
