0be266b9af9311ed1588e7052ce175c402e20193
Tsvi Zandany <szandany@liquibase.com>
2021-08-11 09:12:41 -0500

    added safty validity checks for valid changelog name and changeset unique identifiers

--- modifyChangeSet/modifyChangeSet.sh
+++ modifyChangeSet/modifyChangeSet.sh
@@ -1,5 +1,12 @@
-# ./modifyChangeSet.sh /path/to/changelog.sql label author:id
+#!/bin/bash
+# ./modifyChangeSet.sh /path/to/changelog.sql labelName author:id
 EXAMPLE_COMMAND="./modifyChangeSet.sh /path/to/changelog.sql labelName author:id"
+grep -q "\<$3\>" $1
+retVal=$?
+if [ $retVal -ne 0 ]; then
+    echo "ERROR: Unique identifier $3 doesn't exists."
+    exit $retVal
+fi
 
 if [ -z "$1" ]
 then
@@ -18,5 +25,21 @@ then
     exit 1
 fi
 
+if [ $1 != *".sql" ]
+then
+    echo "ERROR: changelog '$1' can only be of SQL format.";
+    exit 1
+fi
+
+if [[ $3 == *":"* ]]
+then
+    echo "Appending label '$2' to changelog: $1 and changeset: $3";
+else
+    echo "ERROR: changeset unique identifier '$3' is missing the key:value ':' operator.";
+    exit 1
+fi
+
 sed "/${3}/s/$/ labels:${2}/" ${1} > ${1}.temp
+
+# Uncomment below line to overwrite to current changelog with the temp file
 # mv ${1}.temp ${1}
