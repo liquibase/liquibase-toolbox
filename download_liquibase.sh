e68938c92c1a022a403a828883d6bba6adbbee70
szandany <44092058+szandany@users.noreply.github.com>
2021-08-23 21:06:26 -0500

    Update download_liquibase.sh

--- download_liquibase.sh
+++ download_liquibase.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 VERSION=$(curl -s https://github.com/liquibase/liquibase/releases/latest | grep -o "v.*" | sed s/'>.*'//g |  sed s/'v'//g | sed 's/"//g')
+VERSION="${1:-"$VERSION"}"
 curl -L https://github.com/liquibase/liquibase/releases/download/v${VERSION}/liquibase-${VERSION}.zip --output liquibase-${VERSION}.zip
 unzip -o -d liquibase-${VERSION} liquibase-${VERSION}.zip
