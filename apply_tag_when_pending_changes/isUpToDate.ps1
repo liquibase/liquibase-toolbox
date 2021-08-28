6c24b10268893fc107ba66ae733ea6846ba7cd9b
szandany <44092058+szandany@users.noreply.github.com>
2021-08-27 21:05:01 -0500

    Update isUpToDate.ps1

--- apply_tag_when_pending_changes/isUpToDate.ps1
+++ apply_tag_when_pending_changes/isUpToDate.ps1
@@ -3,7 +3,7 @@ Param ( $tag = 'version1')
 $status=(liquibase status | Select-String -Pattern 'is up to date')
 
 if ( $status -like '*is up to date*' ){
-    Write-Output "echo Noting to update; no tag applied."
+    Write-Output "Noting to update; no tag applied."
 }
 else {
     liquibase tag $tag
