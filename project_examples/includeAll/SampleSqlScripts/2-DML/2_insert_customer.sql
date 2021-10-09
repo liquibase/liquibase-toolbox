215e1deb9956bd280f699c3b45bddc2731ce0d81
Tsvi Zandany <44092058+szandany@users.noreply.github.com>
2021-10-09 11:47:48 -0500

    Update 2_insert_customer.sql

--- project_examples/includeAll/SampleSqlScripts/2-DML/2_insert_customer.sql
+++ project_examples/includeAll/SampleSqlScripts/2-DML/2_insert_customer.sql
@@ -1,3 +1,7 @@
+-- liquibase formatted sql
+
+-- changeset DanD:dml_v32
+
 INSERT INTO customer (id, name, birthdate) VALUES (1, 'John Doe', '1960-10-30');
 INSERT INTO customer (id, name, birthdate) VALUES (2, 'Pepito Pérez', '1954-07-15');
 INSERT INTO customer (id, name, birthdate) VALUES (3, 'Cosme Fulanito', '1956-05-12');
