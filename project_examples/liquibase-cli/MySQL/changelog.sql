39910b84c4137391ee429a832f24a219d4fb87cb
Tsvi Zandany <44092058+szandany@users.noreply.github.com>
2021-10-26 15:24:25 -0500

    Update changelog.sql

--- project_examples/liquibase-cli/MySQL/changelog.sql
+++ project_examples/liquibase-cli/MySQL/changelog.sql
@@ -1,6 +1,6 @@
 -- liquibase formatted sql
 
--- changeset SteveZ:1588216 context:"qa and dev" labels:Jira1000,Jira1001
+-- changeset SteveZ:1588216
 CREATE TABLE department 
 (
   id INT NOT NULL, 
@@ -10,17 +10,17 @@ CREATE TABLE department
   ;
 --rollback drop table department;
 
---changeset SteveZ:15834216 context:test labels:Jira1000,Jira1001
+--changeset SteveZ:15834216
 ALTER TABLE department
 ADD Email varchar(255);
 --rollback ALTER TABLE department DROP COLUMN Email;
 
---changeset SteveZ:158fep92 context:stage labels:Jira1000,Jira1001
+--changeset SteveZ:158fep92
 CREATE INDEX idx_department
 ON department (id, active);
 --rollback DROP INDEX idx_department ON department;
 
---changeset SteveZ:1588221 context:stage labels:Jira1000,Jira1001
+--changeset SteveZ:1588221
 CREATE TABLE `book` (
   `id` mediumint(8) unsigned NOT NULL AUTO_INCREMENT,
   `title` varchar(200) NOT NULL,
@@ -30,7 +30,7 @@ CREATE TABLE `book` (
 --rollback drop table book;
 
 
---changeset SteveZ:158821221 context:stage labels:Jira1000,Jira1001
+--changeset SteveZ:158821221
 INSERT INTO `book` (`title`, `tags`)
 VALUES (
   'ECMAScript 2015: A SitePoint Anthology',
@@ -38,7 +38,7 @@ VALUES (
 );
 --rollback DELETE FROM book WHERE title='ECMAScript 2015: A SitePoint Anthology';
 
---changeset SteveZ:21221543 context:stage labels:Jira1000,Jira1001
+--changeset SteveZ:21221543
 CREATE TABLE shop (
     article INT UNSIGNED  DEFAULT '0000' NOT NULL,
     dealer  CHAR(20)      DEFAULT ''     NOT NULL,
@@ -47,7 +47,7 @@ CREATE TABLE shop (
     ;
 --rollback drop table shop;
     
---changeset SteveZ:1588216758 context:stage labels:Jira1000,Jira1001
+--changeset SteveZ:1588216758
 INSERT INTO shop VALUES
     (1,'A',3.45),(1,'B',3.99),(2,'A',10.99),(3,'B',1.45),
     (3,'C',1.69),(3,'D',1.25),(4,'D',19.95);   
@@ -56,7 +56,7 @@ INSERT INTO shop VALUES
 --rollback DELETE FROM shop WHERE article=3;
 --rollback DELETE FROM shop WHERE article=4;
 
---changeset BobR:5_456 SplitStatements:false context:stage labels:Jira1001
+--changeset BobR:5_456 SplitStatements:false
 CREATE PROCEDURE get_shopcount()
 BEGIN
 Select count(*) from shop1;
