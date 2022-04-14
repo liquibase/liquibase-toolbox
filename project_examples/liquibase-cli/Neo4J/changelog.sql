67a23e144b5e70cf1b378f4868d08dc4b5ec5ea5
Tsvi Zandany <44092058+szandany@users.noreply.github.com>
2022-04-14 07:52:49 -0500

    Update changelog.sql

--- project_examples/liquibase-cli/Neo4J/changelog.sql
+++ project_examples/liquibase-cli/Neo4J/changelog.sql
@@ -16,6 +16,31 @@ CREATE (Carrie:Person {name:'Carrie-Anne Moss', born:1967})
 --rollback DELETE n
 
 -- changeset Tsvi:4
+CREATE (Laurence:Person {name:'Laurence fishburne', born:1961})
+--rollback MATCH (n:Person {name: 'Laurence'})
+--rollback DELETE n
+
+-- changeset Tsvi:5
+CREATE (Hugo:Person {name:'Hugo weaving', born:1962})
+--rollback MATCH (n:Person {name: 'Hugo'})
+--rollback DELETE n
+
+-- changeset Tsvi:6
+CREATE (LillyW:Person {name:'Lilly Wachowski', born:1967})
+--rollback MATCH (n:Person {name: 'Lilly'})
+--rollback DELETE n
+
+-- changeset Tsvi:7
+CREATE (LanaW:Person {name:'Lana Wachowski', born:1965})
+--rollback MATCH (n:Person {name: 'Lana'})
+--rollback DELETE n
+
+-- changeset Tsvi:8
+CREATE (JoelS:Person {name:'Joel Silver', born:1952})
+--rollback MATCH (n:Person {name: 'Joel'})
+--rollback DELETE n
+
+-- changeset Tsvi:9
 CREATE
 (Keanu)-[:ACTED_IN {roles:['Neo']}]->(TheMatrix),
 (Carrie)-[:ACTED_IN {roles:['Trinity']}]->(TheMatrix),
